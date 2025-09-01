8cc453b2309dbaec7c1b9cfd43e76015097d2ce3
jedked1313 <jeddo1313@gmail.com>
2025-09-02 00:31:18 +0300

    fix some errors

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,9 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if let key = Bundle.main.object(forInfoDictionaryKey: "MAPS_API_KEY") as? String {
-      GMSServices.provideAPIKey(key)
-    }
+    GMSServices.provideAPIKey("key")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
